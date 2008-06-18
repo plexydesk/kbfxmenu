b488ff6fac64eb2a7e83c3bafa04e9ddb203bcd8
fernando.lopezjr <fernando.lopezjr@4b8e4d9b-5c46-0410-baef-e162252e9dc2>
2008-06-18 20:17:17 +0000

    astyle formatting...

--- trunk/kbfx4/configdialog/kbfx_main.h
+++ trunk/kbfx4/configdialog/kbfx_main.h
@@ -1,6 +1,6 @@
 #ifndef DLGMAIN_H
 #define DLGMAIN_H
- 
+
 #include "ui_kbfx_main.h"
 
 #include "kbfx_themes.h"
@@ -11,35 +11,35 @@
 #include "kbfx_tooltip.h"
 #include "kbfx_mstyle.h"
 #include "kbfx_about.h"
- 
+
 class dlgMain : public QWidget, private Ui::dlgMain
 {
     Q_OBJECT
- 
+
 public:
     dlgMain(QWidget *parent = 0);
- 
+
 public slots:
     void menu_change();
     void apply_btn();
     void closeEvent(QCloseEvent *event);
-		void readSettings();
-		void writeSettings();
-    
-private:
- 		KbfxConfigDlgThemes *pageThemes;
- 		dlgkickerbtn *pagekickerbtn;
- 		dlgfonts *pagefonts;
- 		dlglayout *pagelayout;
- 		dlgplugins *pageplugins;
- 		dlgtooltip *pagetooltip;
- 		dlgmstyle *pagemstyle;
-		dlgabout *pageabout;
+    void readSettings();
+    void writeSettings();
 
-		
-		
+private:
+    KbfxConfigDlgThemes *pageThemes;
+    dlgkickerbtn *pagekickerbtn;
+    dlgfonts *pagefonts;
+    dlglayout *pagelayout;
+    dlgplugins *pageplugins;
+    dlgtooltip *pagetooltip;
+    dlgmstyle *pagemstyle;
+    dlgabout *pageabout;
+
+
+
 
 };
- 
- 
+
+
 #endif
--- trunk/kbfx4/configdialog/kbfx_themes.h
+++ trunk/kbfx4/configdialog/kbfx_themes.h
@@ -1,22 +1,22 @@
 #ifndef KBFX_THEMES_H
 #define KBFX_THEMES_H
- 
+
 #include "ui_kbfx_themes.h"
- 
- 
+
+
 class KbfxConfigDlgThemes : public QWidget, private Ui::KbfxConfigDlgThemes
 {
     Q_OBJECT
- 
+
 public:
     KbfxConfigDlgThemes(QWidget *parent = 0);
- 
- 
+
+
 public slots:
-		void readSettings();
-		void writeSettings();
-		
+    void readSettings();
+    void writeSettings();
+
 };
- 
- 
+
+
 #endif
